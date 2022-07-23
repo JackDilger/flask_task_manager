531616351ec398beffb949ee88eb27df5436b26a
JackDilger <jack.dilger1995@gmail.com>
2022-07-23 17:01:37 +0000

    eror

--- app.py
+++ app.py
@@ -197,3 +197,13 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def error_500(error):
+    return render_template('500.html'), 500
